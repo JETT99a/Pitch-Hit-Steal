bac3e6991cb9fb809d58566fb74847b1b3368d17
JETT99 <47643947+JETT99a@users.noreply.github.com>
2022-02-08 22:39:46 +0000

    Added Beta pitching IF

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-# Project Rust Honey
-## Version 0.1 Beta
+# Version 0.9 Beta
+# Added Beta pitching Interface  
+
 import random
 import time
 
@@ -25,10 +26,10 @@ input("\nPress Enter To Start Game ")
 print("\nTop of the 1st")
 
 while ining != 4:                #game loop
-
- while outs != 3:                #inning loop, change inning once 3 outs
+ 
+ while outs != 3:                #inning loop, change                                inning once 3 outs
    time.sleep(1.5)
-   print("\nBatter steps into the box")
+   print("\nOuts:",outs,"\tBatter steps into the box")
    pitch = random.randint(1,3)
    print("\n\t1. Fastball\n\t2. Curveball\n\t3. Slider")
    sel = int(input("\nSelect the pitch "))
@@ -41,7 +42,7 @@ while ining != 4:                #game loop
      print("\nInfeild Hit! Single")
      strikes = 0
     else:
-      print("\nOutfeild Hit! Double")
+      print("\nOutfeild Hit! Double") #hits to the outfeild = double
       strikes = 0
    else:
     strikes += 1
@@ -53,3 +54,38 @@ while ining != 4:                #game loop
       strikes = 0
       time.sleep(2)
  
+ print("\nSWITCH SIDES")
+ 
+ outs = 0
+ base1 = 0
+ base2 = 0
+ base3 = 0
+
+ while outs != 3:
+   print("You Are Now The Pitcher")
+   time.sleep(1.5)
+   print("\nOuts:",outs,"\tBatter steps into the box")
+   cpu_hitter = random.randint(1,3)
+   print("\n\t1. Fastball\n\t2. Curveball\n\t3. Slider")
+   sel = int(input("\nSelect the pitch you want to throw "))
+  
+   print("\nAnd Heres the pitch")
+   time.sleep(1)
+
+   if cpu_hitter == sel:               #selection, pitch comparison
+    if random.randint(1,2) == 1:   #location selector
+     print("\nInfeild Hit! Single")
+     strikes = 0
+    else:
+      print("\nOutfeild Hit! Double") #hits to the outfeild = double
+      strikes = 0
+   else:
+    strikes += 1
+    print("\nswing and a miss for Stike ",strikes)
+    
+    if strikes == 3:
+      print ("\nStrike Three the batter is out")
+      outs += 1
+      strikes = 0
+      time.sleep(2)
+
