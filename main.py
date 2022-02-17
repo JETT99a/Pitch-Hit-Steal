fd1ce2c7cd4bdef55a64164f9498795b72b911fc
JETT99 <47643947+JETT99a@users.noreply.github.com>
2022-02-17 11:15:39 -0800

    Base running if

--- main.py
+++ main.py
@@ -1,90 +1,112 @@
-
-
 import random
 import time
 
 outs = 0
-strikes = 0 
+strikes = 0
 balls = 0
 ining = 0
 home = 0
 away = 0
-base1 = 0
-base2 = 0
-base3 = 0
+b1 = 0
+b2 = 0
+b3 = 0
 
 print("""
 |=============================|
 | Welcome to Pitch Hit Steal! |
 |=============================|
 
-When promted sellect the pitch you think the pitcher will throw, if you get it correct then you get a hit. if you dont get it correct you miss the ball and a strike is add. while pitching its the same thing execpept you are trying to strink the batter out and not allow a hit.
+When prompted select the pitch you think the pitcher will throw, if you get it correct then you get a hit. if you don't get it correct you miss the ball and a strike is added. while pitching it's the same thing except you are trying to strike the batter out and not allow a hit. If the base says that is it 0 it is unoccupied, but if its 1 then there is a runner occupying the base.
 """)
 
 input("\nPress Enter To Start Game ")
 
-print("\nTop of the 1st")
 
-while ining != 4:                #game loop
- 
- while outs != 3:                #inning loop, change                                inning once 3 outs
-   time.sleep(1.5)
-   print("\nOuts:",outs,"\tBatter steps into the box")
-   pitch = random.randint(1,3)
-   print("\n\t1. Fastball\n\t2. Curveball\n\t3. Slider")
-   sel = int(input("\nSelect the pitch "))
-  
-   print("\nAnd Heres the pitch")
-   time.sleep(1)
 
-   if pitch == sel:               #selection, pitch comparison
-    if random.randint(1,2) == 1:   #location selector
-     print("\nInfeild Hit! Single")
-     strikes = 0
-    else:
-      print("\nOutfeild Hit! Double") #hits to the outfeild = double
-      strikes = 0
-   else:
-    strikes += 1
-    print("\nswing and a miss for Stike ",strikes)
-    
-    if strikes == 3:
-      print ("\nStrike Three your out!")
-      outs += 1
-      strikes = 0
-      time.sleep(2)
- 
- print("\nSWITCH SIDES")
- 
- outs = 0
- base1 = 0
- base2 = 0
- base3 = 0
- print("You Are Now The Pitcher")
- while outs != 3:
-   time.sleep(1.5)
-   print("\nOuts:",outs,"\tBatter steps into the box")
-   cpu_hitter = random.randint(1,3)
-   print("\n\t1. Fastball\n\t2. Curveball\n\t3. Slider")
-   sel = int(input("\nSelect the pitch you want to throw "))
-  
-   print("\nAnd Heres the pitch")
-   time.sleep(1)
+while ining != 4:  #game loop
+    ining += 1
+    while outs != 3:  #inning loop, change                                inning once 3 outs
+        print("\nInning: ", ining, "\tHome: ", home, "\tAway:", away, "\tOuts: ", outs)
+        print("\tFirst Base: ", b1, "\tSecond Base: ", b2, "\tThird Base: ", b3)
+        time.sleep(2.5)
+        print("\nBatter steps into the box")
+        pitch = random.randint(1, 3)
+        print("\n\t1. Fastball\n\t2. Curveball\n\t3. Slider")
+        sel = int(input("\nSelect the pitch "))
 
-   if cpu_hitter == sel:               #selection, pitch comparison
-    if random.randint(1,2) == 1:   #location selector
-     print("\nInfeild Hit! Single")
-     strikes = 0
-    else:
-      print("\nOutfeild Hit! Double") #hits to the outfeild = double
-      strikes = 0
-   else:
-    strikes += 1
-    print("\nswing and a miss for Stike ",strikes)
-    
-    if strikes == 3:
-      print ("\nStrike Three the batter is out")
-      outs += 1
-      strikes = 0
-      time.sleep(2)
+        print("\nAnd Heres the pitch")
+        time.sleep(1)
 
+        if pitch == sel:  #selection, pitch comparison
+         print("HIT!")
+         strikes = 0
+         if b1 == 1:
+           if b2 == 1:
+             if b3 == 1:
+               home += 1 
+               print("You Got 1 run!!")
+             elif b3 == 0:
+               b3 = 1
+           elif b2 == 0:
+             b2 = 1
+         elif b1 == 0:
+           b1 = 1
+      
+        else:
+          strikes += 1
+          print("\nswing and a miss for Stike ", strikes)
+          if strikes == 3:
+                print("\nStrike Three your out!")
+                outs += 1
+                strikes = 0
+                time.sleep(2)
+
+    print("SWITCH SIDES")
+    b1 = 0
+    b2 = 0
+    b3 = 0
+    outs = 0
+    while outs != 3:  #inning loop, change                                inning once 3 outs
+        print("\nInning: ", ining, "\tHome: ", home, "\tAway:", away, "\tOuts: ", outs)
+        print("\tFirst Base: ", b1, "\tSecond Base: ", b2, "\tThird Base: ", b3)
+        time.sleep(1.5)
+        print("\nBatter steps into the box")
+        pitch = random.randint(1, 3)
+        print("\n\t1. Fastball\n\t2. Curveball\n\t3. Slider")
+        sel = int(input("\nSelect the pitch You want the throw"))
+
+        print("\nAnd Heres the pitch")
+        time.sleep(2)
+
+        if pitch == sel:  #selection, pitch comparison
+          print("HIT!")
+          stikes = 0
+          if b1 == 1:
+           if b2 == 1:
+             if b3 == 1:
+               away += 1 
+               print("you let up a run")
+             elif b3 == 0:
+               b3 = 1
+           elif b2 == 0:
+             b2 = 1
+          elif b1 == 0:
+           b1 = 1
+
+        else:
+          strikes += 1
+          print("\nswing and a miss for Stike ", strikes)
+          if strikes == 3:
+                print("\nStrike Three The batter is out")
+                outs += 1
+                strikes = 0
+                time.sleep(2) 
+    print("SWITCH SIDES")
+    b1 = 0
+    b2 = 0
+    b3 = 0
+    outs = 0
+if home >= away:
+  print("You Won!!! \n The Final Score: ","\tHome: ", home, "\tAway:", away)
+elif away >= home:
+  print("You Lost :( \n The Final Score: ","\tHome: ", home, "\tAway:", away)
